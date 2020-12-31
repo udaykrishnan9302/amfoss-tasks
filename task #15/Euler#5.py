5aea34eb44b453e4f353fc0e33e6c40c698aff3e
Uday Krishnan <73242028+udaykrishnan9302@users.noreply.github.com>
2020-12-31 19:17:17 +0530

    Update Euler#5.py

--- task #15/Euler#5.py
+++ task #15/Euler#5.py
@@ -1,9 +1,11 @@
-n=int(input("enter:"))
-p=1
-for i in range(1,n+1):
-  while True:
-    if p%i!=0:
-        p+=1
-    else:
-        print(p)
-        break
+t=int(input())
+for i in range(t):
+  n=int(input())
+  p=1
+  for i in range(1,n+1):
+    while True:
+      if p%i!=0:
+          p+=1
+      else:
+          print(p)
+          break
